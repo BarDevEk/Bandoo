92a4bcab01b694cd4e368f96aaf73336ebb83781
Bartek Kisielewiczgit config --global user.name Bartek <bartkub5574@gmail.com>
2018-05-05 10:00:21 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,9 @@
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Welcome to Bandoo</title>
+</head>
+<body>
+	Hello
+</body>
+</html>
---
+++ register.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Bandoo | Register for free !</title>
+</head>
+<body>
+	<div id="inputContainer">
+		<form action="register.php" id="loginForm" method="POST">
+			<h2>Login to Bandoo</h2>
+			<p>
+				<label for="loginUsername">Username:</label>
+				<input type="text" id="loginUsername" name="loginUsername" required>
+			</p>
+			<p>
+				<label for="loginPassword">Password:</label>
+				<input type="password" id="loginPassword" name="loginPassword" required>
+			</p>
+			<button type="submit" name="loginButton">LOG IN</button>
+		</form>
+	</div>
+</body>
+</html>
